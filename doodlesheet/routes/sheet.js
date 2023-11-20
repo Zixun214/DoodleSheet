a8d0da1e5793bd868f4acf282d8c86ae5274aa0e
nyela20 <ralantonisainananyela@gmail.com>
2023-11-20 12:38:47 +0100

    close sheet

--- doodlesheet/routes/sheet.js
+++ doodlesheet/routes/sheet.js
@@ -2,27 +2,32 @@ var express = require('express');
 var router = express.Router();
 const sheetController = require('../controller/sheetController');
 
-/* GET sheet page. */
+/**
+ * @brief affichage sheet
+ */
 router.get('/:sheetId', async(req, res, next) => {
   var sheetid = req.originalUrl.replace('/sheet/', '');
   var sheetInfo = await sheetController.getSheet(req, res, next, sheetid);
   var sheetContent = await sheetController.getSheetContent(req, res, next, sheetid);
-
-  //console.log(sheetInfo);
-  //console.log(sheetContent);
   res.render('sheet', { title: 'DoodleSheet file',sheetInfo, sheetContent});
 });
 
+/**
+ * @brief sauvegarde sheet
+ */
 router.post('/save/:sheetId', async(req, res, next) => {
   var sheetid = req.originalUrl.replace('sheet/save/', '').replace('/', '');
   await sheetController.savesheet(req, res, next, sheetid);
-  //var sheetContent = await sheetController.getSheetContent(req, res, next, sheetid);
-
-
-  //console.log(sheetInfo);
-  //console.log(sheetContent);
-  //res.render('sheet', { title: 'DoodleSheet file',sheetInfo, sheetContent});
 });
+
+/**
+ * @brief redirection vers la liste des sheets
+ */
+router.post('/close', async(req, res, next) => {
+  res.redirect('/sheets');
+})
+
+
 
 
 module.exports = router;
